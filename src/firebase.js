e04dd8f9ebebb8896b42e131d59f825ec93dca4a
Themmythorpe <soge.tope@gmail.com>
2024-11-18 03:56:24 +0100

    first initial coomit

---
+++ src/firebase.js
@@ -0,0 +1,65 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import {
+  getMessaging,
+  getToken,
+  onMessage,
+  isSupported,
+} from "firebase/messaging";
+import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyCBXUbUerHO1zJ9jkObim-WVzodUjqCMEE",
+  authDomain: "sendeva-633cd.firebaseapp.com",
+  projectId: "sendeva-633cd",
+  storageBucket: "sendeva-633cd.firebasestorage.app",
+  messagingSenderId: "1065391601456",
+  appId: "1:1065391601456:web:7fe380cf9a633e9129faf5",
+  measurementId: "G-6M0R8CMFRN"
+};
+const firebaseApp = !getApps().length
+  ? initializeApp(firebaseConfig)
+  : getApp();
+const messaging = (async () => {
+  try {
+    const isSupportedBrowser = await isSupported();
+    if (isSupportedBrowser) {
+      return getMessaging(firebaseApp);
+    }
+    return null;
+  } catch (err) {
+    return null;
+  }
+})();
+
+export const fetchToken = async (setTokenFound, setFcmToken) => {
+  return getToken(await messaging, {
+    vapidKey: "",
+  })
+    .then((currentToken) => {
+      if (currentToken) {
+        setTokenFound(true);
+        setFcmToken(currentToken);
+
+        // Track the token -> client mapping, by sending to backend server
+        // show on the UI that permission is secured
+      } else {
+        setTokenFound(false);
+        setFcmToken();
+        // shows on the UI that permission is required
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      // catch error while creating client token
+    });
+};
+
+export const onMessageListener = async () =>
+  new Promise((resolve) =>
+    (async () => {
+      const messagingResolve = await messaging;
+      onMessage(messagingResolve, (payload) => {
+        resolve(payload);
+      });
+    })()
+  );
+export const auth = getAuth(firebaseApp);
---
+++ src/utils/staticCredential.js
@@ -0,0 +1,6 @@
+export const google_client_id = "920811422067-gf32sc7hrmr4lljkrgik3oj6tddqcu4j.apps.googleusercontent.com";
+export const fb_app_id = "";
+export const appleLoginCredential = {
+  serviceId: "",
+  redirectURI: ``,
+};
