536e716bbfd48ab6ae0797d0adb77b5de0dd19d0
Themmythorpe <soge.tope@gmail.com>
2025-05-25 12:17:17 +0100

    update from cursor

--- src/utils/staticCredential.js
+++ src/utils/staticCredential.js
@@ -1,4 +1,12 @@
 export const google_client_id =
   "";
 export const fb_app_id = "";
+export const appleLoginCredential = {
+    clientId: "",
+    redirectURI: "",
+    responseType: "code",
+    responseMode: "form_post",
+    usePopup: true,
+    scope: "email name"
+};
 
